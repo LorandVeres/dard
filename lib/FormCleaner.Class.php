7caf0968f5f26265aa9262f390f5576ee16bb80b
LorandVeres <lorand.mast@gmail.com>
2018-02-22 18:26:54 +0000

    Retrieving runtime messages per module and page or just page

--- lib/FormCleaner.Class.php
+++ lib/FormCleaner.Class.php
@@ -4,119 +4,118 @@
  */
 class FormCleaner {
 
-    public $post = FALSE;
-    // $error store an array of error id's
-    protected $errors = array();
-    // $error_msg store an array of error message
-    protected $error_msg = array();
-    protected $is_error = FALSE;
-    private $regex_paswd = "/^[\S\p{L}\p{M}*0-9]{6,50}$/";
-    private $regex_user = "/^(\w){3,24}$/";
-    private $regex_email = "/^([\w\.\-\+]){1,70}@[a-zA-Z0-9\-\.]{3,}[\.]{1}[a-zA-Z]{2,}$/";
-    private $regex_name = "/^[\S\p{L}\p{M}*\s\-\.']{1,35}$/";
+	public $post = FALSE;
+	// $error store an array of error id's
+	protected $errors = array();
+	// $error_msg store an array of error message
+	protected $error_msg = array();
+	protected $is_error = FALSE;
+	private $regex_paswd = "/^[\S\p{L}\p{M}*0-9]{6,50}$/";
+	private $regex_user = "/^(\w){3,24}$/";
+	private $regex_email = "/^([\w\.\-\+]){1,70}@[a-zA-Z0-9\-\.]{3,}[\.]{1}[a-zA-Z]{2,}$/";
+	private $regex_name = "/^[\S\p{L}\p{M}*\s\-\.']{1,35}$/";
 
-    function __construct($config) {
-        $this -> BoleanPost();
-        $this -> utf8_matches($config);
-    }
+	function __construct($config) {
+		$this -> BoleanPost();
+		$this -> utf8_matches($config);
+	}
 
-    private function utf8_matches($config) {
-        if ($config -> language !== "en") {
-            $this -> regex_paswd .= $this -> regex_paswd . "u";
-            $this -> regex_user .= $this -> regex_user . "u";
-        }
-    }
-
-    protected function BoleanPost() {
-        if (isset($_POST) && count($_POST) > 0 && !empty($_POST)) {
-            $this -> post = TRUE;
-        }
-    }
-
-    protected function str_lenght($string, $min, $max) {
-        $result = FALSE;
-        $lenght = strlen($string);
-        if ($lenght >= $min && $lenght <= $max)
-            $result = TRUE;
-        return $result;
-    }
-
-    protected function match($patern, $string, $error_number) {
-        $result = FALSE;
-        if (isset($string)) {
-            if (preg_match($patern, $string)) {
-                $result = TRUE;
-            } else {
-                array_push($this -> errors, $error_number);
-            }
-        } else {
-            array_push($this -> errors, $error_number);
-        }
-        return $result;
-    }
-
-    protected function clean_email($email) {
-        return $this -> match($this -> regex_email, $email, 1);
-    }
-
-    protected function clean_login($email, $passwd) {
-        $this -> match($this -> regex_email, $email, 4);
-        $this -> match($this -> regex_paswd, $passwd, 4);
-    }
-
-    protected function clean_paswd($paswd) {
-        return $this -> match($this -> regex_paswd, $paswd, 2);
-    }
-
-    protected function clean_captcha($captcha) {
-        $result = FALSE;
-        $captcha = trim($captcha);
-        if (isset($_SESSION['captcha']))
-            $captcha === $_SESSION['captcha'] ? $result = TRUE : $result = FALSE;
-        if (!$result)
-            array_push($this -> errors, 3);
-        return $result;
-    }
-
-    protected function generate_query($module) {
-        $arr = array();
-		if(is_array($module)){
-			$arr = $module;
-		}else if(is_string($module)){
-			$arr[0] = $module;
-			$arr[1] = $module;
+	private function utf8_matches($config) {
+		if ($config -> language !== "en") {
+			$this -> regex_paswd .= $this -> regex_paswd . "u";
+			$this -> regex_user .= $this -> regex_user . "u";
 		}
-        $query = "SELECT `message` FROM `error_message` WHERE";
-		$help = "(SELECT `id` FROM `module` WHERE `name` = '$arr[0]')";
-		$help1 = "(SELECT `id` FROM `page` WHERE `pagename` = '$arr[1]')";
-        $where = '';
-        if (count($this -> errors) > 0) {
-            for ($i = 0; $i < count($this -> errors); $i++) {
-                $j = $this -> errors[$i];
-                if ($i == 0) {
-                    $where = "`number` = '$j'";
-                } else {
-                    $where .= " OR `number` = '$j'";
-                }
-            }
-        }
-        return $query .= $where . " AND (`module` = $help OR `page` = $help1);";
-    }
+	}
 
-    protected function retrive_error_msg($config, $DBconect, $module) {
-    	$this -> error_msg = $DBconect -> selectDB($this -> errors, $config, $this -> generate_query($module), FALSE, 'array');
-    }
+	protected function BoleanPost() {
+		if (isset($_POST) && count($_POST) > 0 && !empty($_POST)) {
+			$this -> post = TRUE;
+		}
+	}
 
-    public function html_wrap_errors($config, $DBconect, $tag) {
-        $wrap = array();
-        if ($this -> is_error) {
-            $wrap = $tag -> tag('div', 'id="error_msg"', '');
-            foreach ($this->error_msg as $value) {
-                $tag -> append_tag($wrap, $tag -> tag('p', 'class="error_msg_id"', $value));
-            }
-            $tag -> docOutput($wrap);
-        }
-    }
+	protected function str_lenght($string, $min, $max) {
+		$result = FALSE;
+		$lenght = strlen($string);
+		if ($lenght >= $min && $lenght <= $max)
+			$result = TRUE;
+		return $result;
+	}
+
+	protected function match($patern, $string, $error_number) {
+		$result = FALSE;
+		if (isset($string)) {
+			if (preg_match($patern, $string)) {
+				$result = TRUE;
+			} else {
+				array_push($this -> errors, $error_number);
+			}
+		} else {
+			array_push($this -> errors, $error_number);
+		}
+		return $result;
+	}
+
+	protected function clean_email($email) {
+		return $this -> match($this -> regex_email, $email, 1);
+	}
+
+	protected function clean_login($email, $passwd) {
+		$this -> match($this -> regex_email, $email, 4);
+		$this -> match($this -> regex_paswd, $passwd, 4);
+	}
+
+	protected function clean_paswd($paswd) {
+		return $this -> match($this -> regex_paswd, $paswd, 2);
+	}
+
+	protected function clean_captcha($captcha) {
+		$result = FALSE;
+		$captcha = trim($captcha);
+		if (isset($_SESSION['captcha']))
+			$captcha === $_SESSION['captcha'] ? $result = TRUE : $result = FALSE;
+		if (!$result)
+			array_push($this -> errors, 3);
+		return $result;
+	}
+
+	protected function generate_query($module) {
+		if (is_array($module)) {
+			$help = "(SELECT `id` FROM `module` WHERE `name` = '$module[0]')";
+			$help1 = "(SELECT `id` FROM `page` WHERE `pagename` = '$module[1]')";
+			$help_where = "(`module` = $help OR `page` = $help1)";
+		} else if (is_string($module)) {
+			$help_where = "`page = (SELECT `id` FROM `page` WHERE `pagename` = '$module')";
+		}
+		$query = "SELECT `message` FROM `error_message` WHERE";
+		$where = '';
+		if (count($this -> errors) > 0) {
+			for ($i = 0; $i < count($this -> errors); $i++) {
+				$j = $this -> errors[$i];
+				if ($i == 0) {
+					$where = "`number` = '$j'";
+				} else {
+					$where .= " OR `number` = '$j'";
+				}
+			}
+		}
+		return $query .= $where . " AND " . $help_where . ";";
+	}
+
+	protected function retrive_error_msg($config, $DBconect, $module) {
+		$query = $this -> generate_query($module);
+		$this -> error_msg = $DBconect -> selectDB($this -> errors, $config, $query, FALSE, 'array');
+	}
+
+	public function html_wrap_errors($config, $DBconect, $tag) {
+		$wrap = array();
+		if ($this -> is_error) {
+			$wrap = $tag -> tag('div', 'id="error_msg"', '');
+			foreach ($this->error_msg as $value) {
+				$tag -> append_tag($wrap, $tag -> tag('p', 'class="error_msg_id"', $value));
+			}
+			$tag -> docOutput($wrap);
+		}
+	}
 
 }
 ?>
--- module/user/login.Class.php
+++ module/user/login.Class.php
@@ -1,100 +1,96 @@
 <?php
 include_once '../module/user/User.Class.php';
 
-
 /**
  *
  */
 class login extends User {
 
-    private $mesage = array();
+	private $mesage = array();
 
-    function __construct($config, $DBconect, $tag) {
-        parent::__construct($config, $DBconect);
-        $this -> login_user($config, $DBconect);
-    }
+	function __construct($config, $DBconect, $tag) {
+		parent::__construct($config, $DBconect);
+		$this -> login_user($config, $DBconect);
+	}
 
-    private function check_error_msg() {
-        if (count($this -> errors) > 0) {
-            $this -> is_error = TRUE;
-            return TRUE;
-        }
-    }
+	private function check_error_msg() {
+		if (count($this -> errors) > 0) {
+			$this -> is_error = TRUE;
+			return TRUE;
+		}
+	}
 
-    private function check_inputs($email, $pass, $captcha) {
-        $this -> clean_login($email, $pass);
-        $this -> clean_captcha($captcha);
-    }
+	private function check_inputs($email, $pass, $captcha) {
+		$this -> clean_login($email, $pass);
+		$this -> clean_captcha($captcha);
+	}
 
-    private function insert_failed_login_ip() {
+	private function insert_failed_login_ip() {
 
-    }
+	}
 
-    private function check_failed_ip_number() {
+	private function check_failed_ip_number() {
 
-    }
+	}
 
-    private function failed_login($config, $DBconect) {
-        array_push($this -> errors, 4);
-        $this -> generate_query('user');
-        $this -> retrive_error_msg($config, $DBconect, 'user');
-        $this -> is_error = TRUE;
-    }
+	private function failed_login($config, $DBconect) {
+		array_push($this -> errors, 4);
+		$this -> retrive_error_msg($config, $DBconect, array('main', 'login'));
+		$this -> is_error = TRUE;
+	}
 
-    private function check_user_login_credentials($config, $DBconect) {
-        $email = $_POST['email'];
-        $password = $this -> passw_hassh($_POST['password'], $config);
-        $arg = array($email, $password);
-        $query = "SELECT `id`, `email`, `password`, `u_group`, `firstname` FROM `user` WHERE `email` = '$email';";
-        $cred = $DBconect -> selectDB($arg, $config, $query, TRUE, 'array');
-        if ($cred === NULL || !password_verify($_POST['password'], $cred['password'])) {
-            return FALSE;
-        } else {
-            return $cred;
-        }
-    }
+	private function check_user_login_credentials($config, $DBconect) {
+		isset($_POST['email']) ? $email = $_POST['email'] : $email = '';
+		$query = "SELECT `id`, `email`, `password`, `u_group`, `firstname` FROM `user` WHERE `email` = '$email';";
+		$cred = $DBconect -> selectDB($email, $config, $query, TRUE, 'array');
+		if ($cred === NULL || !password_verify($_POST['password'], $cred['password'])) {
+			return FALSE;
+		} else {
+			return $cred;
+		}
+	}
 
-    private function login_user($config, $DBconect) {
-        if ($this -> post) {
-            $this -> check_inputs($_POST['email'], $_POST['password'], $_POST['captcha']);
+	private function login_user($config, $DBconect) {
+		if ($this -> post) {
+			$this -> check_inputs($_POST['email'], $_POST['password'], $_POST['captcha']);
 
-            //here some more filtering functions would have to come
+			//here some more filtering functions would have to come
 
-            if ($this -> check_error_msg()) {
-                $this -> retrive_error_msg($config, $DBconect, 'user');
-            } else {
-                $is_user = $this -> check_user_login_credentials($config, $DBconect);
-                if (!$is_user) {
-                    $this -> failed_login($config, $DBconect);
-                } else {
-                    $this -> set_session($is_user);
-                }
-            }
-        }
-    }
+			if ($this -> check_error_msg()) {
+				$this -> retrive_error_msg($config, $DBconect, array('main', 'user'));
+			} else {
+				$is_user = $this -> check_user_login_credentials($config, $DBconect);
+				if (!$is_user) {
+					$this -> failed_login($config, $DBconect);
+				} else {
+					$this -> set_session($is_user);
+				}
+			}
+		}
+	}
 
-    private function startSession() {
-        if (session_status() === 1) {
-            include '../../lib/DardSession.Class.php';
-            $_DARDSESSI = new DardSession();
-            return $_DARDSESSI;
-        }
-    }
+	private function startSession() {
+		if (session_status() === 1) {
+			include '../../lib/DardSession.Class.php';
+			$_DARDSESSI = new DardSession();
+			return $_DARDSESSI;
+		}
+	}
 
-    private function set_session($user) {
-        if (!isset($_DARDSESSION))
-            $_DARDSESSION = $this -> startSession();
-        if ($user['firstname'] === NULL) {
-            $email = $user['email'];
-            $username = substr_replace($email, '', strpos($email, '@'));
-        } else {
-            $username = $user['firstname'];
-        }
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['user_name'] = $username;
-        $_SESSION['user_loged'] = TRUE;
-        $_SESSION['user_priv'] = $user['u_group'];
-    }
+	private function set_session($user) {
+		if (!isset($_DARDSESSION))
+			$_DARDSESSION = $this -> startSession();
+		if ($user['firstname'] === NULL) {
+			$email = $user['email'];
+			$username = substr_replace($email, '', strpos($email, '@'));
+		} else {
+			$username = $user['firstname'];
+		}
+		$_SESSION['user_id'] = $user['id'];
+		$_SESSION['user_name'] = $username;
+		$_SESSION['user_loged'] = TRUE;
+		$_SESSION['user_priv'] = $user['u_group'];
+	}
 
 }
 ?>
