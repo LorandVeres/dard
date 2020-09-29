64e7aef1439fe61f285dd186b09ccdf24c47fac4
LorandVeres <lorand.mast@gmail.com>
2020-09-29 01:26:49 +0100

    User login menu in top sticker

--- lib/functions.php
+++ lib/functions.php
@@ -1,13 +1,13 @@
 <?php
-if(!defined('DARDSTATUS') ) exit();
+if (!defined('DARDSTATUS'))
+	exit();
 
-
-function check_and_do($to_be_checked, $do){
-    if($to_be_checked){
-        $do;
-    }else{
-        return ;
-    }
+function check_and_do($to_be_checked, $do) {
+	if ($to_be_checked) {
+		$do;
+	} else {
+		return;
+	}
 }
 
 // $http_status_code 301 Moved Permanently
@@ -16,26 +16,64 @@ function check_and_do($to_be_checked, $do){
 // $http_status_code 307 Temporary Redirect
 // $http_status_code 403 Forbiden
 
-function redirect($myPage, $where, $http_status_code){
-    array_push ($myPage, header("Location:".$where, TRUE, $http_status_code));
+function redirect($myPage, $where, $http_status_code) {
+	array_push($myPage, header("Location:" . $where, TRUE, $http_status_code));
 }
 
-function mylink($link){
-    if($link === '/')$link = ''; 
-    $url = '';
-    $host = $_SERVER['HTTP_HOST'];
-    $_SERVER["HTTPS"] == "on" ? $url .= 'https://'.$host : $url .= 'http://'.$host ;
-    $url .= '/'.$link;
-    printf("%s", $url);
-    
+function mylink($link) {
+	if ($link === '/')
+		$link = '';
+	$url = '';
+	$host = $_SERVER['HTTP_HOST'];
+	$_SERVER["HTTPS"] == "on" ? $url .= 'https://' . $host : $url .= 'http://' . $host;
+	$url .= '/' . $link;
+	printf("%s", $url);
+
 }
 
-function postecho($value){
-	if(isset($_POST[$value])){
+function postecho($value) {
+	if (isset($_POST[$value])) {
 		echo $_POST[$value];
-	}else{
+	} else {
 		return;
 	}
 }
 
+function mysql_human_date($value, $date_type) {
+	$date = '';
+	//if (is_array($value)) {
+		switch ($date_type) {
+			case 'h' :
+				$a = explode('-', $value);
+				if(count($a) == 3)
+					$date = $a[2] . '/' . $a[1] . '/' . $a[0];
+				break;
+
+			case 'm' :
+				$a = explode('/', $value);
+				if(count($a) == 3)
+					$date = $a[2] . '-' . $a[1] . '-' . $a[0];
+				break;
+		}
+	//}
+	return $date;
+}
+
+function print_user_sign_in($tag) {
+	$_SESSION['user_loged'] ? $user = $_SESSION['user_name'] : $user = 'Account';
+	$login_div = $tag -> tag('div', 'class="col-r user-account-logins"', '');
+	$tag -> append_tag($login_div, $tag -> tag('a', 'href="javascript:void(0)" class="user-account-name" onclick="toggle_login_menu()"', $user));
+	$user_panel = $tag -> tag('div', 'class="user-account-logins-panel"', '');
+	// If the user is not logged in
+	if ($user === 'Account') {
+		$tag -> append_tag($user_panel, $tag -> tag('a', 'href="/login"', 'Sign in'));
+		$tag -> append_tag($user_panel, $tag -> tag('a', 'href="/register"', 'Register'));
+	} else {
+		$tag -> append_tag($user_panel, $tag -> tag('a', 'href="/account"', 'Account'));
+		$tag -> append_tag($user_panel, $tag -> tag('a', 'href="/logout"', 'Logout'));
+	}
+	//$tag -> append_tag($user_panel, $tag -> tag('a', 'href="recover-password"', 'Forgotten password'));
+	$tag -> append_tag($login_div, $user_panel);
+	$tag -> print_doc($login_div, 4);
+}
 ?>
--- www/template/layout/main/top-sticker.php
+++ www/template/layout/main/top-sticker.php
@@ -1,7 +1,12 @@
-        <div id="top-sticker" class="section group">
-            <ul id="top-menu" class="left">
-                <li>
-                    <span id="menuicon">&#x2261;</span>
-                </li>
-            </ul>
-        </div>
+<?php
+if(!defined('DARDSTATUS'))
+	exit ;
+?>
+			<div id="top-sticker" class="section group">
+				<ul id="top-menu" class="left">
+					<li>
+						<span id="menuicon">&#x2261;</span>
+					</li>
+				</ul>
+<?php print_user_sign_in($tag); ?>
+			</div>
