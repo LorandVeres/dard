ae488580e303a7a81c5e6ffb9aa072b1b634b5e7
LorandVeres <lorand.mast@gmail.com>
2021-12-09 02:52:42 +0000

    Combo boxes re designed for Module page

--- module/main/modules.Class.php
+++ module/main/modules.Class.php
@@ -30,21 +30,16 @@ class modules extends FormCleaner {
 	 */
 	public function print_modules_combo_boxes($dard, $tag) {
 		$modules_all = $this -> select_all_modules($dard);
-		$box = $tag -> tag('div', 'class="section group row_8 center_box"', '');
+		$box = $tag -> tag('div', 'class="section group max_row_9 center_box"', '');
 		for ($i = 0; $i < count($modules_all); $i++) {
 			$module_id = $modules_all[$i]["id"];
 			$col = $tag -> tag('div', 'class="module_comp_box"', '');
-			$h = $tag -> tag('h3', '', 'Module : ' . $modules_all[$i]["name"]);
-			$p = $tag -> tag('p', '', $modules_all[0]["description"]);
-			$link_div = $tag -> tag('div', 'class="section group center" style="margin-right:20px"', '');
-			$link1 = $tag -> tag('a', 'href="modules?a=add-page" class="col_r spacer_1"', 'Add page');
-			$link2 = $tag -> tag('a', 'href="modules?a=view-pages" class="col_r spacer_1"', 'View pages');
-			$link3 = $tag -> tag('a', 'href="pages/error-messages?a=view&moduleid='.$modules_all[$i]["id"].'" class="col_r spacer_1"', 'Run time mesages');
-			$tag -> append_tag($link_div, $link1);
-			$tag -> append_tag($link_div, $link2);
-			$tag -> append_tag($link_div, $link3);
-			$tag -> append_tag($col, $h);
-			$tag -> append_tag($col, $p);
+			$link_div = $tag -> tag('div', 'class="section group center"', '');
+			$tag -> append_tag($link_div, $tag -> tag('a', 'href="modules?a=add-page&moduleid='. $module_id .'" class="col_r spacer_1"', 'Add page'));
+			$tag -> append_tag($link_div, $tag -> tag('a', 'href="modules?a=view-pages&moduleid' .$module_id. '" class="col_r spacer_1"', 'View pages'));
+			$tag -> append_tag($link_div, $tag -> tag('a', 'href="pages/error-messages?a=view&moduleid='. $module_id. '" class="col_r spacer_1"', 'Run time mesages'));
+			$tag -> append_tag($col, $tag -> tag('h3', '', $modules_all[$i]["name"]));
+			$tag -> append_tag($col, $tag -> tag('p', '', $modules_all[0]["description"]));
 			$tag -> append_tag($col, $link_div);
 			$tag -> append_tag($box, $col);
 		}
--- www/template/module/main/modules.php
+++ www/template/module/main/modules.php
@@ -9,9 +9,9 @@ $dard->ifNoAjaxTop($tag);
 				<div class="template">
 				</div>
 				<header class="row section group">
-					<div class="left top">
-						<h1 class="left spacer_3">Modules <span></span></h1>
-					</div>
+					<h1 class="col spacer_3"><a href="/modules">Modules</a></h1>
+					<a href="modules?a=add_module" class="left head-link">Add Module</a>
+					<a href="modules?a=import_module" class="left head-link" onclick="alert('Importing functionality not yet implemented')">Import Module</a>
 					<div class="search_box livesearch spacer_1">
 						<input id="searchbox" type="search" size="30" onkeyup="searchPageId(this.value)" placeholder="search page">
 						<div id="searchlinks">
@@ -19,5 +19,8 @@ $dard->ifNoAjaxTop($tag);
 					</div>
 				</header>
 <?php $dard_modules -> print_modules_combo_boxes($dard, $tag); ?>
+			
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu </p>
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu </p>
 			</div>
 <?php $dard -> ifNoAjaxBottom(); ?>
