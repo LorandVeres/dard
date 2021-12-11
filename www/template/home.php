0a58a28c90c4b467e4f11e49d59cbd70ca056d53
LorandVeres <lorand.mast@gmail.com>
2021-12-11 23:17:03 +0000

    Minor css adjustments, eliminate doble footer from home page

--- www/template/home.php
+++ www/template/home.php
@@ -2,7 +2,7 @@
         <div id="main" class="section group">
             <?php include_once 'template/module/live/layout/top-sticker.php';  ?>
             <div id="content">
-                <header>
+                <header class="row section group">
                     <h1>Dard CMS project</h1>
                 </header>
                 <div>
@@ -11,9 +11,3 @@
 
             </div>
         </div>
-        
-        <footer>
-            <p>
-                &copy; 2021 Copyright  by dard.ovh
-            </p>
-        </footer>
