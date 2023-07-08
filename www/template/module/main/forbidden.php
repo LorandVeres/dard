d987b153d17ec105557a1cc04afede516bf655c6
Lorand Veres <lorand.mast@gmail.com>
2023-07-08 02:25:39 +0100

    Small changes to the forbiden page

--- www/template/module/main/forbidden.php
+++ www/template/module/main/forbidden.php
@@ -1,10 +1,9 @@
             <div id="h">
                 <h1>403 Forbidden!</h1>
                 <p class="h">
-                    You don't have permision to access. Your session may be expired.
-                </p>
-                <p class="h">
-                    If you have an acounnt with us please <a href="/login">Login to your acount</a>
+					It seams you don't have permision to access this page.<br><br>
+					If you have an account with us: <br><br>Your session may be expired due to lack of interactions with the page for a longer time.<br>
+					Please <a href="/login">Log in to your account</a>
                 </p>
                 <div id="d">
                     <p>
