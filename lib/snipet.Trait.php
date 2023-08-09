fb6e25abfc6a48d4e85aac2161fdff51eddf14f3
Lorand Veres <lorand.mast@gmail.com>
2023-08-09 23:30:40 +0100

    snipet_json_to_html handles arrays of blocks

--- lib/snipet.Trait.php
+++ lib/snipet.Trait.php
@@ -21,23 +21,31 @@ trait snipetHandler{
             }
             return $attr;
         };
+        
+        $tag_block = function ($snipet, $tag) use (&$set_attributes, &$new_tag){
+            if(isset($snipet['e_type'])){
+				if(is_string($snipet['e_content'])){
+					$new_tag = $tag -> tag(($snipet['e_name'] !== '#text' ? strtolower($snipet['e_name']) : ''), (isset($snipet['e_attr']) ? $set_attributes($snipet['e_attr']) : ''), $snipet['e_content']);
+				}elseif(is_array($snipet['e_content'])){
+					$new_tag = $tag -> tag(strtolower($snipet['e_name']), $set_attributes($snipet['e_attr']), '');
+					foreach ($snipet['e_content'] as $value) {
+						if(is_array($value)){
+							if(count($value) >=1){
+								$tag -> append_tag($new_tag, $this -> snipet_json_to_html($value, $tag));
+							}
+						}else{
+							if (is_string($value))
+								$tag ->append_tag($new_tag, $tag -> tag('', '', $snipet['e_content']));
+						}
+					}
+				}
+			}
+        };
         if(isset($snipet['e_type'])){
-            if(is_string($snipet['e_content'])){
-                $new_tag = $tag -> tag(($snipet['e_name'] !== '#text' ? strtolower($snipet['e_name']) : ''), (isset($snipet['e_attr']) ? $set_attributes($snipet['e_attr']) : ''), $snipet['e_content']);
-            }elseif(is_array($snipet['e_content'])){
-                $new_tag = $tag -> tag(strtolower($snipet['e_name']), $set_attributes($snipet['e_attr']), '');
-                foreach ($snipet['e_content'] as $value) {
-                    if(is_array($value)){
-                        if(count($value) >=1){
-                            $tag -> append_tag($new_tag, $this -> snipet_json_to_html($value, $tag));
-                            //var_dump($value);
-                        }
-                    }else{
-                        //var_dump($value);
-                        if (is_string($value))
-                            $tag ->append_tag($new_tag, $tag -> tag('', '', $snipet['e_content']));
-                    }
-                }
+            $tag_block($snipet, $tag);
+        }else{
+            foreach ( $snipet as $value){
+				$tag_block($value, $tag);
             }
         }
         return $new_tag;
